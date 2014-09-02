4f7801b0db7f9cc976461563aa8e246a7f8b1466
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-09-02 16:08:15 +0800

    [REV] Update manage file

--- souche/manage.py
+++ souche/manage.py
@@ -3,14 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    import socket
-    hostname = socket.gethostname()
-    SETTINGS_FILE = {
-        "gongpingjia": "souche.settings.product",
-        "eyelee": "souche.settings.test"
-    }
-    settings_file = SETTINGS_FILE.get(hostname, "souche.settings.dev")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_file)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
 

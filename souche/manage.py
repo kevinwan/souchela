8d8b67c3601ad47c8b190b88aabbfb160c573810
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-09-22 16:02:45 +0800

    [REV] Update manage file

--- souche/manage.py
+++ souche/manage.py
@@ -2,7 +2,12 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
+    project_path = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), os.pardir))
+    if project_path not in sys.path:
+        sys.path.append(project_path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
